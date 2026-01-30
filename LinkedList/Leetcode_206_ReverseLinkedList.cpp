d014497f6c5a7b39fec5ce445948451602255fe5
Panchal Priyansh <panchalpriyansh3257@gmail.com>
2026-01-30 22:20:22 +0530

    reverse linked list

---
+++ LinkedList/Leetcode_206_ReverseLinkedList.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* reverseList(ListNode* head) {
+    ListNode* prev = NULL;
+    ListNode* curr = head;
+
+    while (curr != NULL) {
+        ListNode* next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    return prev;
+}
+
+void printList(ListNode* head) {
+    while (head != NULL) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+
+    cout << "Original List: ";
+    printList(head);
+
+    head = reverseList(head);
+
+    cout << "Reversed List: ";
+    printList(head);
+
+    return 0;
+}
